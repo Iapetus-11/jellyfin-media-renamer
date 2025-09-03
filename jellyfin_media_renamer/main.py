fb98fc86c780e296fec3c6d60156d53b58295725
Milo Weinberg <iapetus011@gmail.com>
2025-09-03 18:59:20 -0400

    Start on season/show processing

--- jellyfin_media_renamer/main.py
+++ jellyfin_media_renamer/main.py
@@ -1,4 +1,5 @@
 import enum
+import os
 import re
 import sys
 from pathlib import Path
@@ -26,8 +27,8 @@ class CommandError(Exception):
         self.message = message
 
 
-def get_name_and_year(fp: Path) -> tuple[str, int | None]:
-    name, year = infer_name_and_year(fp)
+def get_name_and_year(fp: Path) -> tuple[str, str, int | None]:
+    raw_name, name, year = infer_name_and_year(fp)
 
     print(f"Detected name: {name}")
     print(f"Detected year: {year}")
@@ -36,17 +37,24 @@ def get_name_and_year(fp: Path) -> tuple[str, int | None]:
 
     if not is_correct:
         return (
-            input("Enter correct name: "),
+            input_str := input("Enter correct name: "),
+            input_str,
             int(input("Enter year (optional): ") or 0) or None,
         )
 
-    return name.strip(), year
+    return raw_name, name.strip(), year
 
 
-def infer_name_and_year(fp: Path) -> tuple[str, int | None]:
+def strip_tags(text: str) -> str:
+    return re.sub(
+        r"(\[[a-zA-Z0-9\-_\+\.\s\$\#\@\!]+\])", "", text
+    )  # Remove tags like [1080p]
+
+
+def infer_name_and_year(fp: Path) -> tuple[str, str, int | None]:
     name = fp.name
     if fp.is_file():
-        name = name[: len(fp.suffix)]
+        name = name[: -len(fp.suffix)]
 
     # Find the year and split the title by it (we don't care for tags/junk after the year)
     year = next(re.finditer(r"\(([0-9]{4})\)", fp.name), None)
@@ -62,12 +70,12 @@ def infer_name_and_year(fp: Path) -> tuple[str, int | None]:
             name = name.split(f".{year}")[0]
             year = int(year)
 
-    name = re.sub(r"\.+(\w+)", r" \1", name)  # Replace dots with spaces
-    name = re.sub(
-        r"(\[[a-zA-Z0-9\-_\+\.\s\$\#\@\!]+\])", "", name
-    )  # Remove tags like [1080p]
+    raw_name = strip_tags(name).strip()
 
-    return name.strip(), year
+    name = re.sub(r"\.+(\w+)", r" \1", name)  # Replace dots with spaces
+    name = strip_tags(name)
+
+    return raw_name, name.strip(), year
 
 
 class InputType(str, enum.Enum):
@@ -101,6 +109,153 @@ def infer_input_type(fp: Path) -> InputType:
     raise CommandError(f"Failed to determine MediaType for path: {fp}")
 
 
+def process_movie_without_folder(fp: Path, name: str, year: int, new_stem: str):
+    folder = fp.parent / new_stem
+    folder.mkdir()
+    fp.rename(folder / fp.with_name(new_stem).with_suffix(fp.suffixes[-1]).name)
+
+
+def purge_extra_files(folder: Path):
+    known_exts = {f".{ext}" for ext in [*VIDEO_FILE_EXTS, *SUBTITLES_FILE_EXTS]}
+    extra_files = [
+        f for f in folder.iterdir() if f.is_file() and f.suffix not in known_exts
+    ]
+
+    if not extra_files:
+        return
+
+    print("Purge extra files?\n", "\n".join(f"\t{f}" for f in extra_files), "\n")
+
+    if input("[Y/n]: ").upper() in ["Y", "YES", "YE", ""]:
+        for file in extra_files:
+            os.remove(file.absolute())
+
+
+def process_movie_inside_folder(fp: Path, name: str, year: int, new_stem: str):
+    fp = fp.rename(fp.with_name(new_stem))
+
+    sub_objs = {f for f in fp.iterdir() if f.is_file()}
+    video_files = {
+        f for f in sub_objs if f.suffixes and f.suffixes[-1][1:] in VIDEO_FILE_EXTS
+    }
+    subtitle_files = {
+        f for f in sub_objs if f.suffixes and f.suffixes[-1][1:] in SUBTITLES_FILE_EXTS
+    }
+
+    if unprocessable_paths := (sub_objs - video_files - subtitle_files):
+        print(
+            "Unprocessable paths:\n",
+            "\n".join(f"\t{f}" for f in unprocessable_paths),
+            "\n",
+        )
+
+    assert len(video_files) >= 1
+
+    # Sometimes torrents include a preview or some message from the uploader
+    primary_video_file: Path | None = None
+    if len(video_files) == 1:
+        primary_video_file = next(iter(video_files))
+    else:
+        for file in video_files:
+            _, test_name, _ = infer_name_and_year(file)
+            if test_name.upper() == name.upper():
+                primary_video_file = file
+                break
+
+    if primary_video_file is None:
+        raise CommandError(f"Unable to determine movie file inside path: {fp}")
+
+    primary_video_file.rename(
+        primary_video_file.with_name(new_stem).with_suffix(
+            primary_video_file.suffixes[-1]
+        )
+    )
+
+    primary_subtitles_file: Path | None = None
+    if len(subtitle_files) == 1:
+        primary_subtitles_file = next(iter(subtitle_files))
+    else:
+        for file in subtitle_files:
+            _, test_name, _ = infer_name_and_year(file)
+            if test_name.upper() == name.upper():
+                primary_subtitles_file = file
+                break
+
+    if primary_subtitles_file:
+        primary_subtitles_file.rename(
+            primary_subtitles_file.with_name(new_stem).with_suffix(
+                "."
+                + ".".join(
+                    [s.removeprefix(".") for s in primary_subtitles_file.suffixes]
+                )
+            )
+        )
+    elif len(subtitle_files):
+        print("Couldn't determine primary subtitles file :/")
+
+    purge_extra_files(fp)
+
+
+def infer_episode_number_and_name(
+    fp: Path, raw_show_name: str, show_name: str, year: int, season: int
+) -> tuple[int, str | None]:
+    assert fp.is_file()
+
+    re_patterns = [
+        (r"episode\s(\d+)", 1),  # Episode 01
+        (r"S\d{1,2}E(\d{1,3})", 1),  # S01E01
+        (r"ep(\d{1,3})", 1),  # Ep01
+        (rf"{season}x(\d{{1,3}})(?:\s|$|\.|\[|\(|\,|_)", 1),  # {season}x01
+        (rf"(?:^|\s|\.){season}(\d{{2,3}})(?:\s|\.|$|_|-)", 1),  # {season}01
+        (r"(?:^|\s|\.|_|-)((?:0\d)|(?:[1-9]\d))(?:\s|\.|$|_|-)", 1),  # 01
+    ]
+
+    ep_number: int | None = None
+    match: re.Match[str] | None = None
+    for pattern, cap_group in re_patterns:
+        if match := next(re.finditer(pattern, fp.name, re.IGNORECASE), None):
+            ep_number = int(match.group(cap_group).strip())
+            break
+
+    if ep_number is None:
+        raise CommandError(f"Unable to determine episode number for path {fp}")
+
+    name = fp.name[: -len(fp.suffix)].replace(raw_show_name, "").replace(show_name, "").strip()
+    name = strip_tags(name)
+    name = name.replace(match.group(), '', 1)  # Remove ep number
+
+    for re_pattern in [
+        r'((?:\(|\[|\s|-|\.)\d{4}(?:\)|\]|\s|-|\.))',  # Year
+        r'(?:^|\s|\.|-|_)(ep|episode)(?:$|\s|\.|-|_)',  # ep | episode
+        r'(\((?:(?:1080)|(?:480)|(?:720)|(?:2160))p.*\))',  # (1080p ...)
+    ]:
+        name = re.sub(re_pattern, "", name, re.IGNORECASE)
+
+    name = name.strip(',.-_ ')
+
+    return ep_number, name or None
+
+
+def process_show_season(folder: Path, show_name: str, year: int):
+    ...
+
+    purge_extra_files(folder)
+
+
+def process_show(fp: Path, raw_name: str, name: str, year: int, new_stem: str):
+    fp = fp.rename(fp.with_name(new_stem))
+
+    for file in fp.iterdir():
+        if file.is_dir() and "SEASON" in file.name.upper():
+            season_num = next(re.finditer(r"(\d{1,2})\s|$", file.name), None)
+            if season_num is None:
+                raise CommandError(f"Unable to determine season number for {file}")
+            season_num = int(season_num.group(1))
+
+            season_folder = file.rename(file.with_name(f"Season {season_num:02d}"))
+            process_show_season(season_folder, name, year)
+
+
 def main():
     raw_path = " ".join(sys.argv[1:])
 
@@ -115,78 +270,22 @@ def main():
     input_type = infer_input_type(fp)
     print(f"Processing {input_type} at {fp.absolute()} ...")
 
-    name, year = get_name_and_year(fp)
+    raw_name, name, year = get_name_and_year(fp)
 
     new_stem = name
     if year:
         new_stem += f" ({year})"
 
     if input_type == InputType.MOVIE_WITHOUT_FOLDER:
-        fp.rename(fp.with_name(new_stem).with_suffix(fp.suffixes[-1]))
-        return
+        process_movie_without_folder(fp, name, year, new_stem)
 
     if input_type == InputType.FOLDER_WITH_MOVIE:
-        fp = fp.rename(fp.with_name(new_stem))
+        process_movie_inside_folder(fp, name, year, new_stem)
 
-        sub_objs = {f for f in fp.iterdir() if f.is_file()}
-        video_files = {
-            f for f in sub_objs if f.suffixes and f.suffixes[-1][1:] in VIDEO_FILE_EXTS
-        }
-        subtitle_files = {
-            f
-            for f in sub_objs
-            if f.suffixes and f.suffixes[-1][1:] in SUBTITLES_FILE_EXTS
-        }
+    if input_type == InputType.FOLDER_WITH_SHOW_SEASONS:
+        process_show(fp, raw_name, name, year, new_stem)
 
-        if unprocessable_paths := (sub_objs - video_files - subtitle_files):
-            print(
-                "Unprocessable paths:\n",
-                "\n".join(f"\t{f}" for f in unprocessable_paths),
-                "\n",
-            )
-
-        assert len(video_files) >= 1
-
-        # Sometimes torrents include a preview or some message from the uploader
-        primary_video_file: Path | None = None
-        if len(video_files) == 1:
-            primary_video_file = next(iter(video_files))
-        else:
-            for file in video_files:
-                test_name, _ = infer_name_and_year(file)
-                if test_name.upper() == name.upper():
-                    primary_video_file = file
-                    break
-
-        if primary_video_file is None:
-            raise CommandError(f"Unable to determine movie file inside path: {fp}")
-
-        primary_video_file.rename(
-            primary_video_file.with_name(new_stem).with_suffix(
-                primary_video_file.suffixes[-1]
-            )
-        )
-
-        primary_subtitles_file: Path | None = None
-        if len(subtitle_files) == 1:
-            primary_subtitles_file = next(iter(subtitle_files))
-        else:
-            for file in subtitle_files:
-                test_name, _ = infer_name_and_year(file)
-                if test_name.upper() == name.upper():
-                    primary_subtitles_file = file
-                    break
-
-        if primary_subtitles_file:
-            primary_subtitles_file.rename(
-                primary_subtitles_file.with_name(new_stem).with_suffix(
-                    primary_subtitles_file.suffixes[-1]
-                )
-            )
-        elif len(subtitle_files):
-            print("Couldn't determine primary subtitles file :/")
-
-        print('Done!')
+    print("Done!")
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,54 +3,223 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from jellyfin_media_renamer.main import infer_input_type, infer_name_and_year, InputType
+from jellyfin_media_renamer.main import infer_episode_number_and_name, infer_input_type, infer_name_and_year, InputType
 
 
 @pytest.mark.parametrize(
-    ("fp", "expected_name", "expected_year"),
+    ("path", "expected_raw_name", "expected_name", "expected_year"),
     [
-        (Path("./Milo"), "Milo", None),
-        (Path("./[AC] Kamisama Kiss"), "Kamisama Kiss", None),
+        ("./Milo", "Milo", "Milo", None),
+        ("./[AC] Kamisama Kiss", "Kamisama Kiss", "Kamisama Kiss", None),
         (
-            Path("./Harry Potter and the Chamber of Secrets (2002) [1080p]"),
+            "./Harry Potter and the Chamber of Secrets (2002) [1080p]",
+            "Harry Potter and the Chamber of Secrets",
             "Harry Potter and the Chamber of Secrets",
             2002,
         ),
         (
-            Path(
-                "./My Little Pony Equestria Girls - Friendship Games (2015) [1080p] [BluRay] [5.1] [YTS.MX]"
-            ),
+            "./My Little Pony Equestria Girls - Friendship Games (2015) [1080p] [BluRay] [5.1] [YTS.MX]",
+            "My Little Pony Equestria Girls - Friendship Games",
             "My Little Pony Equestria Girls - Friendship Games",
             2015,
         ),
         (
-            Path(
-                "./My.Little.Pony.Equestria.Girls.2013.1080p.BluRay.x264-PHOBOS[rarbg]"
-            ),
+            "./My.Little.Pony.Equestria.Girls.2013.1080p.BluRay.x264-PHOBOS[rarbg]",
+            "My.Little.Pony.Equestria.Girls",
             "My Little Pony Equestria Girls",
             2013,
         ),
         (
-            Path(
-                "./Harry Potter and the Deathly Hallows Part 2 (2011) 1080p.BRrip.scOrp.sujaidr (pimprg)"
-            ),
+            "./Harry Potter and the Deathly Hallows Part 2 (2011) 1080p.BRrip.scOrp.sujaidr (pimprg)",
+            "Harry Potter and the Deathly Hallows Part 2",
             "Harry Potter and the Deathly Hallows Part 2",
             2011,
         ),
         (
-            Path(
-                "./Ouran High School Host Club (2006) [1080p x265 HEVC 10bit BluRay Dual Audio AAC] [Prof][darkflux fixed]"
-            ),
+            "./Ouran High School Host Club (2006) [1080p x265 HEVC 10bit BluRay Dual Audio AAC] [Prof][darkflux fixed]",
+            "Ouran High School Host Club",
             "Ouran High School Host Club",
             2006,
         ),
+        (
+            "./Malcolm in the Middle (2000) (1080p AMZN WEB-DL x265 Silence)",
+            "Malcolm in the Middle",
+            "Malcolm in the Middle",
+            2000,
+        ),
+        (
+            "./[Erai-raws] Death Note [1080p][Multiple Subtitle][BCE68CE7]",
+            "Death Note",
+            "Death Note",
+            None,
+        )
     ],
 )
-def test_infer_name_and_year(fp: Path, expected_name, expected_year):
-    name, year = infer_name_and_year(fp)
+@pytest.mark.parametrize("is_file", [True, False])
+def test_infer_name_and_year(
+    is_file, path, expected_raw_name, expected_name, expected_year
+):
+    def make_path_mock(path_str: str) -> MagicMock:
+        if is_file:
+            path_str += ".mkv"
 
+        path = Path(path_str)
+
+        mock = MagicMock(spec=path)
+        mock.is_file.return_value = is_file
+        mock.suffix = path.suffix
+        mock.name = path.name
+
+        return mock
+
+    raw_name, name, year = infer_name_and_year(make_path_mock(path))
+
+    assert raw_name == expected_raw_name
     assert name == expected_name
     assert year == expected_year
+
+
+@pytest.mark.parametrize(
+    (
+        "show_path",
+        "path",
+        "season",
+        "expected_ep_number",
+        "expected_ep_name",
+    ),
+    [
+        (
+            "The Suite Life of Zack and Cody/",
+            "The Suite Life of Zack and Cody - 1x01 - Hotel Hangout.mkv",
+            1,
+            1,
+            "Hotel Hangout",
+        ),
+        (
+            "[Erai-raws] Death Note [1080p][Multiple Subtitle][BCE68CE7]/",
+            "[Erai-raws] Death Note - 01 [1080p][Multiple Subtitle][BCE68CE7].mkv",
+            0,
+            1,
+            None,
+        ),
+        (
+            "Death Note 1080p Hybrid ITA BDRip DTS-HD-MA 2.0 Kira [SEV]/",
+            "Death Note - Episode 01 - 1,28 1080p Hybrid ITA BDRip DTS-HD-MA 2.0 Kira [SEV].mkv",
+            0,
+            1,
+            "1,28",
+        ),
+        (
+            "Malcolm in the Middle (2000) (1080p AMZN WEB-DL x265 Silence)/",
+            "Malcolm in the Middle (2000) - S07E22 - Graduation (1080p AMZN WEB-DL x265 Silence).mkv",
+            7,
+            22,
+            "Graduation",
+        ),
+        (
+            "Test Show/",
+            "Test Show S01E01.mkv",
+            1,
+            1,
+            None,
+        ),
+        (
+            "Test Show/",
+            "Test Show S01E01 Episode Name.mkv",
+            1,
+            1,
+            "Episode Name",
+        ),
+        (
+            "Test Show (2025)/",
+            "Test Show (2025) S01E01 Episode Name.mkv",
+            1,
+            1,
+            "Episode Name",
+        ),
+        (
+            "Test Show (2025)/",
+            "Test Show (2025) - S01E01 - Episode Name.mkv",
+            1,
+            1,
+            "Episode Name",
+        ),
+        (
+            "[Exiled-Destiny]_Maid-Sama!/",
+            "[Exiled-Destiny]_Maid-Sama!_Ep16v2_(A46BDC49).mkv",
+            0,
+            16,
+            None,
+        ),
+        (
+            "One-Punch Man/",
+            "One-Punch Man - 08 [BDRip 1080p AVC][FLAC].mkv",
+            1,
+            8,
+            None,
+        ),
+        (
+            "Invincible/",
+            "S01E01-I'm Used to It [28559867].mkv",
+            1,
+            1,
+            "I'm Used to It",
+        ),
+        (
+            "NCIS (1234)/",
+            "NCIS S01E01 Yankee White.mkv",
+            1,
+            1,
+            "Yankee White",
+        ),
+        (
+            "Naruto Shippuden/",
+            "[Koten_Gars] Naruto Shippuden - 154 [iTunes][h.264][1080p][AC3] [A5D2B724].mkv",
+            1,
+            54,
+            None,
+        ),
+        (
+            "Invincible (2021)/",
+            "Invincible (2021) - S01E02 - Here Goes Nothing (1080p WEB-DL x265 SAMPA).mkv",
+            1,
+            2,
+            "Here Goes Nothing",
+        ),
+        (
+            "./[AC] Kamisama Kiss/",
+            "[AC] Kamisama Kiss - 04 [BD][1080p-Hi10][FLAC][Dual-Audio][1231231].mkv",
+            1,
+            4,
+            None,
+        )
+    ],
+)
+def test_infer_episode_number_and_name(
+    show_path, path, season, expected_ep_number, expected_ep_name
+):
+    def make_path_mock(str_path: str) -> MagicMock:
+        path = Path(str_path)
+
+        mock = MagicMock(spec=path)
+        mock.is_file.return_value = not str_path.endswith('/')
+        mock.suffix = path.suffix
+        mock.name = path.name
+
+        return mock
+
+    raw_show_name, show_name, show_year = infer_name_and_year(make_path_mock(show_path))
+
+    ep_number, ep_name = infer_episode_number_and_name(
+        make_path_mock(path),
+        raw_show_name,
+        show_name,
+        show_year,
+        season,
+    )
+
+    assert ep_number == expected_ep_number
+    assert ep_name == expected_ep_name
 
 
 @pytest.mark.parametrize(
